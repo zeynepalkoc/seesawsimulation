a258d276847f619174d36ed999fb7e26e700e822
zeynep <142154399+zeynepalkoc@users.noreply.github.com>
2026-03-15 11:18:10 +0300

    add weight ball creation on plank click

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+let objects = [];
+
 const plank=document.getElementById("plank");
 const leftWeight=document.getElementById("leftweight");
 const rightWeight=document.getElementById("rightweight");
@@ -11,7 +13,7 @@ function generateRandomWeight(){
 
 let currentWeight= generateRandomWeight();
 
-nextWeight.textContent = `${currentWeight} kg`;
+
 
 plank.addEventListener("click", handlePlankClick);
 
@@ -28,12 +30,59 @@ function handlePlankClick(event){
    ball.style.height = `${size}px`;
 
    ball.style.left = `${clickX - size/2}px`;
-   ball.style.top = `-50px`;
+    ball.style.top = `-50px`;
+
+  const center = rect.width / 2;
+  const distance = clickX - center;
+
+  objects.push({ weight: currentWeight, distance: distance });
    
    objectsContainer.appendChild(ball);
 
+   const angle = calculateAngle();
+   plank.style.transform=`transtateY(-50%) rotate(${angle}deg)`;
+   tiltAngle.textContent= `${angle.toFixed(1)}°`;
+  
+   updateWeightDisplay();
    console.log("ball added");
+   currentWeight=generateRandomWeight();
+  nextWeight.textContent = `${currentWeight} kg`;
 
 }
 
+function calculateAngle(){
+    let leftTorque=0;
+    let rightTorque=0;
+
+    objects.forEach(function(obj){
+        const distance =Math.abs(obj.distance);
+if(obj.distance<0){
+    leftTorque += obj.weight * distance;
+}else{
+    rightTorque += obj.weight * distance;
+}
+    });
+    const difference = rightTorque - leftTorque;
+    let angle= difference / 10;
+if(angle>30) angle=30;
+if(angle<-30) angle=-30;
+return angle;
+}
+
+ function updateWeightDisplay(){
+    let leftWeightTotal=0;
+    let rightWeightTotal=0;
+
+    objects.forEach(function(obj){
+        if(obj.distance<0){
+            leftWeightTotal += obj.weight;
+
+        }
+        else{
+            rightWeightTotal += obj.weight;
+        }
+    });
+    leftWeight.textContent = ` ${leftWeightTotal} kg`;
+    rightWeight.textContent = ` ${rightWeightTotal} kg`;
+}
  
